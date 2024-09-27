6d4af70419ee0a3fe0833655abb6ef02f704fb3c
Dr.  Christy Henzler <chenzler@ahl04.agate.msi.umn.edu>
2024-09-27 10:52:11 -0500

    snakemake_memory_modifications

--- run_align_scorer.snake.sh
+++ run_align_scorer.snake.sh
@@ -6,4 +6,6 @@ source /home/hsiehph/shared/bin/initialize_conda.sh
 
 conda activate snakemake
 
-snakemake -s align_scorer.snake  --jobname "{rulename}.{jobid}" --profile profile -w 100 --jobs 100 -p -k
+snakemake -s align_scorer.snake  --jobname "{rulename}.{jobid}" --profile profile \
+  --groups run_align_scorer_on_all_in_subdir=group0 --group-components group0=16 \
+  -w 100 --jobs 100 -p -k
